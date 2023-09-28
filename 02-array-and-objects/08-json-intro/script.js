c87da815a1e15688b30cd330e33086171f3add4d
Kim Bum Jun <admin@vivabj.com>
2023-09-29 04:17:28 +0900

    Complete 02 section

--- 02-array-and-objects/08-json-intro/script.js
+++ 02-array-and-objects/08-json-intro/script.js
@@ -8,9 +8,32 @@ const post = {
 
 // Convert to JSON string
 const str = JSON.stringify(post); // Object to Json
+console.log(str.id); //-> undefined
 
 // Parse JSON
 const obj = JSON.parse(str); // Json to Object
+console.log(obj.id); //-> 1
 
+const posts = [
+  {
+    id: 1,
+    title: 'Post One',
+    body: 'This is the body',
+  },
+  {
+    id: 2,
+    title: 'Post Two',
+    body: 'This is the body',
+  },
+  {
+    id: 3,
+    title: 'Post Three',
+    body: 'This is the body',
+  },
+];
+
+const str2 = JSON.stringify(posts);
+
+console.log(str2);
 console.log(str);
 console.log(obj);
