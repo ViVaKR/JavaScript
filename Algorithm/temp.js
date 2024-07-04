b46d05c4ccd545e7924c7618df932e9acf9bdacc
Kim Bum Jun <admin@vivabj.com>
2024-07-04 17:10:29 +0900

    Remove Cached

--- Algorithm/temp.js
+++ Algorithm/temp.js
@@ -1,13 +1,21 @@
 
-let Menu = [0, 1, 2, 3];
-
-Menu.forEach(x => {
-    if (x > 0)
-        console.log(x);
-});
-console.log();
-for (const key in Menu) {
-    const element = Menu[key];
-    if (element > 0)
-        console.log(element);
+// 버블 정렬 (람다)
+function mySort(arr, lambda) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (lambda(arr[j], arr[j + 1]) < 0) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
 }
+
+let arr = [7, 37, 3, 97, 10, 13, 2, 21, 5];
+let lambda = (a, b) => b - a;
+let sorted = mySort(arr, lambda);
+sorted.forEach(x => {
+    console.log(x);
+});
